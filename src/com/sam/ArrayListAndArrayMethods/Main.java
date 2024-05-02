ddfcee546408b68befc5546e751e34e081f8a3cf
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-05-02 17:43:41 -0400

    GLAB Array list and ArrayList methods

---
+++ src/com/sam/ArrayListAndArrayMethods/Main.java
@@ -0,0 +1,41 @@
+package com.sam.ArrayListAndArrayMethods;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Example 1: Creating an ArrayList and Adding New Elements
+
+//        CreateArrayListExample createArrayListExample = new CreateArrayListExample();
+//        createArrayListExample.createArrayListEx();
+
+
+        //Example 2: Creating an ArrayList From Another Collection
+
+//        CreateArrayListFromCollectionExample arrayListExample = new
+//                              CreateArrayListFromCollectionExample();
+
+        //Example 3: Accessing Elements from an ArrayList
+
+//        AccessElementsFromArrayListExample accessElementsFromArrayListExample = new
+//                                        AccessElementsFromArrayListExample();
+//        accessElementsFromArrayListExample.accessElements();
+
+        //Example 4: Removing Elements from an ArrayList
+//
+//        RemoveElementsFromArrayList removeElementsFromArrayList = new
+//                                    RemoveElementsFromArrayList();
+//        removeElementsFromArrayList.removeElements();
+
+        //Example 5: Iterating over an ArrayList
+
+//        IterateOverArrayList list = new IterateOverArrayList();
+//        list.iterator();
+
+        //Example 6: Searching for Elements in an ArrayList
+
+        SearchElementsInArrayListExample se=new SearchElementsInArrayListExample();
+        se.searchElements();
+    }
+    }
